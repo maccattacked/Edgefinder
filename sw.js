cb81e0be9fee68ce3b0188326bc7105afbdefd7b
maccattacked <140094958+maccattacked@users.noreply.github.com>
2026-07-16 11:52:20 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-const CACHE="edgefinder-v1";const ASSETS=["./","./index.html","./manifest.json"];
+const CACHE="edgefinder-v2-footballdata";const ASSETS=["./","./index.html","./manifest.json"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
